904280832cb60b6a29142b6fe3a7e92d9f0944d5
theCityCR <120992342+theCityCR@users.noreply.github.com>
2023-06-08 14:40:20 -0400

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,25 +1,57 @@
 package culminating;
 
 /**
- * 
  * @author Alex Li
- *
+ * 
+ *         Teacher: Mrs. Krasteva
+ * 
+ *         Date: 2023-05-15
+ * 
+ *         This class is the cards.
  */
+
 public class Card {
+	/**
+	 * the information
+	 */
     private String info;
+    
+    /**
+     * whether read
+     */
     private Boolean read;
+    
+    /**
+     * Creates a new Card object
+     * 
+     * @param info	information
+     */
     public Card(String info){
         this.info = info;
         read = false;
     }
 
+    /**
+     * Read card
+     */
     public void hasRead(){
         read = true;
     }
 
+    /**
+     * Whether read
+     * 
+     * @return	whether card has been read
+     */
     public Boolean getRead(){
         return read;
     }
+    
+    /**
+     * Gets the information
+     * 
+     * @return	the information
+     */
     public String getInfo(){
         return info;
     }
